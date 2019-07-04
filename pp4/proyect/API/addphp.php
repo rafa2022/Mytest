4221b63419f1a47d90f5ddb394e0ba06249ef63a
rafa2022 <rafagiga2022@gmail.com>
2019-07-04 15:51:06 -0500

    sin errores de compilacion de parte de angular y con conexion exitosa a DB psql con PDO

---
+++ pp4/proyect/API/addphp.php
@@ -0,0 +1,7 @@
+<?php
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $cell= $_POST['cell'];
+    echo $name, $age, $cell;
+   echo "hola".$name;
+?>
---
+++ pp4/proyect/API/store.php
@@ -0,0 +1,53 @@
+<?php
+require 'conexion.php';
+
+// Get the posted data.
+$postdata = file_get_contents("php://input");
+
+if(isset($postdata) && !empty($postdata))
+{
+  // Extract the data.
+  $request = json_decode($postdata);
+	
+
+  // Validate.
+  if(trim($request->data->name) === '')
+  {
+    return http_response_code(400);
+  }
+	
+  // Sanitize.
+  //$model = mysqli_real_escape_string($con, trim($request->data->model));
+  //$name = $con,trim($request->data->name);  
+  //$price = mysqli_real_escape_string($con, (int)$request->data->price);
+
+  // Store.
+  $sql = "INSERT INTO contacto (name,age,cell) VALUES ('{name}',{age},'{cellphone}')";
+  if($result = pg_query($conn,$sql))
+  {
+    http_response_code(201);
+    $contacto = [
+      'name' => $name,
+      'age' => $age,
+      'cellphone' => $cellphone
+    ];
+    echo json_encode(['data'=>$contacto]);
+  } else {
+    http_response_code(422);
+  }
+
+  /*if(mysqli_query($con,$sql))
+  {
+    http_response_code(201);
+    $car = [
+      'name' => $model,
+      'age' => $price,
+      'cellphone'    => mysqli_insert_id($con)
+    ];
+    echo json_encode(['data'=>$car]);
+  }
+  else
+  {
+    http_response_code(422);
+  }*/
+}
